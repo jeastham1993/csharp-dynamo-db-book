cee75852ad097f4ff524ca4ab54bc62e2289e88d
James Eastham <j.eastham1993@gmail.com>
2020-05-01 21:31:28 +0100

    [FEATURE] Add UML diagrams

---
+++ src/ECommerce/docs/DynamoDbBook.ECommerce.Erd/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using PlantUml.Net;
+
+namespace DynamoDbBook.ECommerce.Erd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new RendererFactory();
+
+            var renderer = factory.CreateRenderer(new PlantUmlSettings());
+
+            var bytes = renderer.RenderAsync(@"skinparam linetype ortho
+  skinparam packageStyle rectangle
+  skinparam shadowing false
+  skinparam class {
+    BackgroundColor White
+    BorderColor Black
+    ArrowColor Black
+  }
+  hide members
+  hide circle
+
+  Customers ||-down-|{ Addresses
+  Customers ||-right-|{ Orders
+  Orders ||-down-|{ OrderItems", OutputFormat.Png).Result;
+            
+            File.WriteAllBytes("erd.png", bytes);
+        }
+    }
+}
